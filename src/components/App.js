5d43b011bcb97a5e4e223a6474e772393aecc77a
eva-lm <eva.lm1810@gmail.com>
2019-12-14 17:04:48 +0100

    add fetch and save data

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,36 @@
 import React from "react";
-import "../stylesheets/App.css";
+import getDataFromServer from "../services/data";
 
-function App() {
-  return <div className="App">holiiiiii</div>;
+import "../stylesheets/App.css";
+import Button from "@material-ui/core/Button";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      spells: []
+    };
+  }
+  componentDidMount() {
+    getDataFromServer().then(data => {
+      console.log("soy los datos de la api", data);
+      this.setState({
+        spells: data
+      });
+    });
+  }
+
+  render() {
+    console.log("soy el estado", this.state);
+    return (
+      <div className="App">
+        <Button variant="contained" color="primary">
+          Hola Mundo!
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/services/data.js
@@ -0,0 +1,8 @@
+const url =
+  "https://www.potterapi.com/v1/spells?key=$2a$10$W4Y/r9zaxMfJVdSv/V9U3.6ETMSveALxjAQy9duMzjpaISSQbU8ty";
+
+const getDataFromServer = () => {
+  return fetch(url).then(response => response.json());
+};
+
+export default getDataFromServer;
