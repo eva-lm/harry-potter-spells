65ac03252110678d696d8cc2ec55d059a751a48d
eva-lm <eva.lm1810@gmail.com>
2019-12-28 17:30:05 +0100

    fix polygon

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,37 +1,38 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import "../stylesheets/polygon.css";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
-import { red } from "@material-ui/core/colors";
-//import polygon from "../stylesheets/polygon.css";
+import { cromo } from "../images/cromo.jpg";
 
 const CharacterCard = props => {
   const { character } = props;
   const useStyles = makeStyles(theme => ({
     card: {
       maxWidth: 345,
-      shapeOutside: polygon(
-        "32px 126px, 163px 32px, 293px 127px, 244px 282px, 82px 281px"
-      )
-      // position: "relative",
-      // background: "transparent",
-      // width: 150,
-      // borderWidth: "120px 58px 0",
-      // borderStyle: "solid",
-      // borderColor: red,
-      // marginTop: "150px",
-      // before: {
-      //   top: "-230px",
-      //   left: "-58px",
-      //   borderWidth: "0 133px 110px",
-      //   borderStyle: "solid"
-      // }
+      backgroundImage: { cromo },
+      backgroundColor: "pink",
+      position: "relative",
+      width: "155px",
+      margin: "20px",
+      borderWidth: "120px 58px 0",
+      borderStyle: "solid",
+      borderColor: "red transparent"
+    },
+    "&::before": {
+      content: "",
+      position: "absolute",
+      height: 0,
+      width: 0,
+      top: "-230px",
+      left: "-58px",
+      borderWidth: "0 133px 110px",
+      borderStyle: "solid",
+      backgroundColor: "blue"
     },
 
     // card:before: {
-
-    // },
 
     media: {
       height: "100px",
@@ -54,61 +55,64 @@ const CharacterCard = props => {
   }));
   const classes = useStyles();
   return (
-    // <div className="polygon">
-    <Card className={classes.card}>
-      <CardHeader title={character.name} subheader={character.house} />
-      <CardMedia
-        className={classes.media}
-        image={character.image}
-        alt={character.name}
-        title="Character photo"
-      />
-      <p>{character.dateOfBirth}</p>
-      <p>
-        {character.ancestry.charAt(0).toUpperCase() +
-          character.ancestry.slice(1)}
-      </p>
+    <React.Fragment>
+      {/* <div className="polygon">A</div> */}
 
-      <p>
-        {character.species.charAt(0).toUpperCase() + character.species.slice(1)}
-      </p>
-      <ul>
-        Wand:
-        <li>
-          {character.wand.wood.charAt(0).toUpperCase() +
-            character.wand.wood.slice(1)}
-        </li>
-        <li>
-          {character.wand.core.charAt(0).toUpperCase() +
-            character.wand.core.slice(1)}
-        </li>
-        <li>
-          {character.wand.length == 0 ? "" : character.wand.length + " cm"}
-        </li>
-      </ul>
-      <p>
-        {character.patronus == 0
-          ? ""
-          : "Patronus: " +
-            character.patronus.charAt(0).toUpperCase() +
-            character.patronus.slice(1)}
-      </p>
-      <p>
-        {character.eyeColour == 0
-          ? ""
-          : "Eyes: " +
-            character.eyeColour.charAt(0).toUpperCase() +
-            character.eyeColour.slice(1)}
-      </p>
-      <p>
-        {character.hairColour == 0
-          ? ""
-          : "Hair: " +
-            character.hairColour.charAt(0).toUpperCase() +
-            character.hairColour.slice(1)}
-      </p>
-    </Card>
-    // </div>
+      <Card className={classes.card}>
+        <CardHeader title={character.name} subheader={character.house} />
+        <CardMedia
+          className={classes.media}
+          image={character.image}
+          alt={character.name}
+          title="Character photo"
+        />
+        <p>{character.dateOfBirth}</p>
+        <p>
+          {character.ancestry.charAt(0).toUpperCase() +
+            character.ancestry.slice(1)}
+        </p>
+
+        <p>
+          {character.species.charAt(0).toUpperCase() +
+            character.species.slice(1)}
+        </p>
+        <ul>
+          Wand:
+          <li>
+            {character.wand.wood.charAt(0).toUpperCase() +
+              character.wand.wood.slice(1)}
+          </li>
+          <li>
+            {character.wand.core.charAt(0).toUpperCase() +
+              character.wand.core.slice(1)}
+          </li>
+          <li>
+            {character.wand.length == 0 ? "" : character.wand.length + " cm"}
+          </li>
+        </ul>
+        <p>
+          {character.patronus == 0
+            ? ""
+            : "Patronus: " +
+              character.patronus.charAt(0).toUpperCase() +
+              character.patronus.slice(1)}
+        </p>
+        <p>
+          {character.eyeColour == 0
+            ? ""
+            : "Eyes: " +
+              character.eyeColour.charAt(0).toUpperCase() +
+              character.eyeColour.slice(1)}
+        </p>
+        <p>
+          {character.hairColour == 0
+            ? ""
+            : "Hair: " +
+              character.hairColour.charAt(0).toUpperCase() +
+              character.hairColour.slice(1)}
+        </p>
+      </Card>
+    </React.Fragment>
   );
 };
 
