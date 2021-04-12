fc647c3e9f440f1a8efac42df1a74e80acb2034c
John Doe <eva.lm1810@gmail.com>
2021-04-12 20:16:43 +0200

    fix code

--- src/services/data.js
+++ src/services/data.js
@@ -2,7 +2,7 @@ const url =
   "https://www.potterapi.com/v1/spells?key=$2a$10$W4Y/r9zaxMfJVdSv/V9U3.6ETMSveALxjAQy9duMzjpaISSQbU8ty";
 
 const getDataFromServer = () => {
-  return fetch(url).then(response => response.json());
+  return fetch(url).then(response => response.json()).catch(err => console.log("err", err));
 };
 
 export default getDataFromServer;
