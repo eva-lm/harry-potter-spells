52ed4c4cf9e94ae725b3046cc7308b2e470815c2
John Doe <eva.lm1810@gmail.com>
2021-08-01 21:48:55 +0200

    styles result quiz

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,6 +6,8 @@ import SlytherinImg from "../images/houses/slytherin.jpg"
 import MuggleImg from "../images/houses/muggle.jpg"
 import sombreroImg from "../images/sombrero.png";
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from "clsx";
+import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Typography from "@material-ui/core/Typography";
 import Radio from '@material-ui/core/Radio';
@@ -15,6 +17,12 @@ import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormLabel from '@material-ui/core/FormLabel';
 import Button from '@material-ui/core/Button';
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import IconButton from "@material-ui/core/IconButton";
 import quiz from "../components/quiz/quiz.json"
 
 
@@ -35,48 +43,81 @@ const [hidden, setHidden] = useState(true);
 const [loading, setLoading] = useState(false)
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: "130px",
+  },
   formControl: {
     margin: theme.spacing(3),
   },
+  radio: {
+    marginTop: "15px",
+  },
+  label: {
+    lineHeight : "25px",
+  },
   button: {
     margin: theme.spacing(1, 1, 0, 0),
+    marginTop: "15px",
   },
   hidden: {
     display: "none",
   },
-  show: {
-    display: "block",
-  },
-  title: {
-    textTransform: "toUpperCase"
-  },
   hatButton: {
    margin: theme.spacing(1, 1, 0, 0),
    backgroundColor: "transparent",
-   width: "50%"
+   width: "100%"
   },
   hatContainer: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    width: "100%",
-  },
-  box: {
-    padding: "50px 0",
-    marginTop: "150px",
-    width: "80%"
   },
   question: {
     width: "60%",
-    marginTop: "150px",
     display: "flex",
     flexDirection: "column",
     alignItems: "center"
   },
   icon : {
     width: "200px"
-  }
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  wrap: {
+    backgroundColor: "#cfcfcf",
+    display: "flex",
+    flexDirection: "column",
+    padding: 10,
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  card: {
+    width: "100%",
+    height: 480,
+    marginTop: 10,
+    padding: 10,
+    display: "flex"
+  },
+  media: {
+    objectFit: "contain",
+    height: "100%",
+  },
 }));
+const [expanded, setExpanded] = useState(false);
+const [expandedSecond, setExpandedSecond] = useState(false);
+
+const handleExpandClick = () => {
+  setExpanded(!expanded);
+};
+const handleExpandClickSecond = () => {
+  setExpandedSecond(!expandedSecond);
+};
 const classes = useStyles();
 
 const handleRadioChange = (event) => {
@@ -130,10 +171,10 @@ const showResults = () => {
     const biggerNumber = Math.max(gryffindor, slytherin, ravenclaw, hufflepuff, muggle);
 
 return (
-  <>
+  <Box className={classes.root}>
   {goQuiz === false ?
   (
-  <Container className={classes.box}>
+  <Container className={classes.question}>
   <Typography variant="h1" component="h2" color="primary" style={{ fontSize: "28px", marginTop: "30px", marginBottom: "30px", textAlign: "center", margin: "50px"}} variant="h2">¡Ceremonia de Selección!</Typography>
   <Container className={classes.hatContainer}>
     <Button         
@@ -160,12 +201,12 @@ return (
   </Typography>
   <form onSubmit={handleSubmit} name={value} id={parseInt(quiz[currentPage].id)} className={hidden === false ? classes.hidden : classes.show}>
         <FormControl component="fieldset" error={error} className={classes.formControl}>
-    <FormLabel component="legend">{quiz[currentPage].question}</FormLabel>
+    <FormLabel className={classes.label} component="legend">{quiz[currentPage].question}</FormLabel>
     <FormHelperText>{helperText}</FormHelperText>
       {quiz[currentPage].res.map((item, index) => {
         return (
           <RadioGroup aria-label="quiz" name="quiz" value={value} onChange={handleRadioChange} key={index}>
-          <FormControlLabel value={item.option} name={item.hause} control={<Radio />} label={item.option} /> 
+          <FormControlLabel className={classes.radio} value={item.option} name={item.hause} control={<Radio />} label={item.option} /> 
           </RadioGroup>
         )
       })}
@@ -179,22 +220,144 @@ return (
   ) : ""
 }
 
-<div className={hidden === true ? classes.hidden : classes.show}>
-    <h2>El sombrero está decidiendo que....... </h2>
+<Container className={hidden === true ? classes.hidden : classes.question}>
+<Typography style={{ fontSize: "20px", textAlign: "center", marginTop: "20px", marginBottom: "30px" }} variant="h2">El sombrero está decidiendo que... </Typography>
   {loading ?
   (
     <>
-  <h3 className={classes.title}>
-    {biggerNumber === gryffindor ? "Perteneces a GRYFFINDOR!!!" : biggerNumber === slytherin ? "Perteneces a SLYTHERIN" : biggerNumber === ravenclaw ? "Perteneces a RAVENCLAW" : biggerNumber === hufflepuff ? "Perteneces a HUFFLEPUFF" : biggerNumber === muggle ? "Eres un muggle!!" : ""}!!!!
-  </h3>
-    <img src={biggerNumber === gryffindor ? GryffindorImg : biggerNumber === slytherin ? SlytherinImg : biggerNumber === ravenclaw ? RavenclawImg : biggerNumber === hufflepuff ? HufflepuffImg : biggerNumber === muggle ? MuggleImg : ""} alt="" />
-      <h4>Afinidad con el resto de casas:</h4>
-        <p>Gryffindor: {gryffindor}%</p>
-        <p>Slytherin: {slytherin}%</p>
-        <p>Ravenclaw: {ravenclaw}%</p>
-        <p>Hufflepuff: {hufflepuff}%</p>
-        <p>Muggle: {muggle}%</p>
-          <Button onClick={()=> {
+    {biggerNumber === gryffindor ? 
+      <Typography style={{ fontSize: "30px", textAlign: "center", marginTop: "15px", marginBottom: "15px" }} variant="h3">
+        Perteneces a <span style={{ color: "#980606" }}><strong>GRYFFINDOR</strong></span>!!
+      </Typography> :
+    biggerNumber === slytherin ? 
+      <Typography style={{ fontSize: "30px", textAlign: "center", marginTop: "15px", marginBottom: "15px" }} variant="h3">
+        Perteneces a <span style={{ color: "#0f6701" }}><strong>SLYTHERIN</strong></span>!!
+      </Typography> :
+    biggerNumber === ravenclaw ? 
+    <Typography style={{ fontSize: "30px", textAlign: "center", marginTop: "15px", marginBottom: "15px" }} variant="h3">
+    Perteneces a <span style={{ color: "#103687" }}><strong>RAVENCLAW</strong></span>!!
+    </Typography> :
+    biggerNumber === hufflepuff ? 
+    <Typography style={{ fontSize: "30px", textAlign: "center", marginTop: "15px", marginBottom: "15px" }} variant="h3">
+    Perteneces a <span style={{ color: "#d4af37" }}><strong>HUFFLEPUFF</strong></span>!!
+    </Typography> : 
+    biggerNumber === muggle ? 
+    <Typography style={{ fontSize: "30px", textAlign: "center", marginTop: "15px", marginBottom: "15px" }} variant="h3">
+    Eres un <span style={{ color: "black" }}><strong>MUGGLE</strong></span>!!
+    </Typography> :  ""}
+
+  <Card className={classes.card}>
+    <div style={{ width: "70%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+  <img
+    className={classes.media}
+    src= {biggerNumber === gryffindor ? GryffindorImg : biggerNumber === slytherin ? SlytherinImg : biggerNumber === ravenclaw ? RavenclawImg : biggerNumber === hufflepuff ? HufflepuffImg : biggerNumber === muggle ? MuggleImg : ""} 
+    alt="escudo casa resultado test"
+    title="Escudo casa"
+    style={{ borderRadius: "12px" }} 
+  />
+    </div>
+    <div style={{ padding: "10px" }}>
+     {biggerNumber === gryffindor ?
+          <Typography variant="body2" color="textPrimary" component="p">
+              Dicen que somos egocéntricos, pero la envidia es muy mala. Todo el mundo sabe que Gryffindor es la mejor casa y que por eso somos los favoritos. Puede que a veces nos guste llamar la atención pero es que solemos brillar más que el resto.
+              Un auténtico Gryffindor te va a defender y no te dejará tirado/a. Si tienes lo que hay que tener, esta es tu casa.
+          </Typography>
+      : biggerNumber === slytherin ?
+          <Typography variant="body2" color="textPrimary" component="p">
+              Nos llaman elítistas y creidos, pero es que admiten a cualquiera en Hogwarts. A parte de ser sangre limpia, tendrás que pasar de cierta gentuza para encajar en Slytherin. Por supuesto, los Weasley dan pereza y Gryffindor apesta. El resto de las casas no están a nuestra altura, pero de vez en cuando les dejamos ganar la Copa por pena.
+      </Typography> 
+      : biggerNumber === ravenclaw ?
+      <Typography variant="body2" color="textPrimary" component="p">
+              Lo malo de ser mas inteligente que el resto, es que en seguida te aburres. Asi que si buscas gente a tu altura, esta es tu casa. Pasas de los dramas Potter-Malfoy, sabes que ambos son muy básicos y te importa tres pimientos si sus casas reciben la copa o no. Un Ravenclaw se siente orgulloso/a de ser quién es y de haber nacido con más neuronas que el resto.
+      </Typography> 
+      : biggerNumber === hufflepuff ? 
+      <Typography variant="body2" color="textPrimary" component="p">
+              Toda la gente maja y con buen rollo es bienvenida en esta casa. Aquí no nos metemos en líos, paz y amor. El resto nos ven como la casa mas débil, pero ¿acaso no se dan cuenta de que vivimos al lado de las cocinas? Por algo será que un buen Hufflepuff siempre monta las mejores fiestas.
+      </Typography>  
+      : biggerNumber === muggle ?
+      <Typography variant="body2" color="textPrimary" component="p">
+            ¡No te desanimes! La mayoría de personas aún seguimos esperando la carta de Hogwarts. Mientras tanto mas te vale que estudies algo útil :)
+      </Typography>       
+          : "" }
+    {biggerNumber !== muggle ? 
+    <>
+    <div style={{ display: "flex", alignItems: "center", borderTop: "0.5px solid #cfcfcf", marginTop: "25px"}}>
+      <Typography variant="body2" color="textSecondary" component="p"> Info sobre tu casa: 
+      </Typography>
+        <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+      </div>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <div className={classes.wrap}>
+            {biggerNumber === gryffindor ?
+            <div>
+            <p>La Casa <strong>Gryffindor</strong> fue fundada por Godric Gryffindor. Sus principios son: <strong>valentía, disposición, coraje y caballerosidad</strong>, ya que éstas son las cualidades de un auténtico Gryffindor debe tener. 
+            - Sus colores representativos son el dorado y el escarlata.
+            - Su símbolo es un león. 
+            - Elemento: fuego.
+            La reliquia de esta casa es espada de Godric Gryffindor y su sala común se encuentra en la Torre  ubicada en el séptimo piso del Castillo de Hogwarts.</p>
+            </div>
+            : biggerNumber === slytherin ? 
+            <div>
+              <p>La Casa <strong>Slytherin</strong>, fundada por el mago Salazar Slytherin y se caracteriza principalmente por la <strong>ambición y la astucia</strong>. 
+              - Sus colores representativos son verde y plateado. 
+              - El animal de esta casa es la serpiente.
+              - Su elemento es el agua.
+              La Sala Común se encuentra situada en las mazmorras, pasando por un serie de numerosos pasillos subterráneos. Se sitúa debajo del Lago Negro, es hace que su sala común sea fría y con un aire verdoso, ya que hay ventanas que dan a las aguas. Su principal reliquia es el guardapelo de Salazar Slytherin. </p>
+            </div>
+            : biggerNumber === ravenclaw ? 
+            <div>
+              <p>La Casa <strong>Ravenclaw</strong> busca alumnos académicos, estudiosos y que siempre sepan lo que hay que hacer. Se rigen por la inteligencia y la creatividad. Fue fundada por la bruja, Rowena Ravenclaw, causante de que las escaleras del castillo se muevan.
+              Su sala común se encuentra en una torre en el ala oeste del castillo. En la entrada se encuentra una estatua con forma de águila hace dice acertijos y sólo te deja entrar si lo resuelves. 
+              - Sus colores representativos son el azul y el bronce. 
+              - El animal de esta casa es el águila, aunque en alguna versión del escudo es un cuervo.
+              Su principal reliquia es la diadema de Rowena Ravenclaw. </p>
+            </div>
+              : biggerNumber === hufflepuff ?
+            <div>
+              <p>La Sala Común de Hufflepuff se encuentra en una bodega en el mismo pasillo subterráneo que la cocina. Inicialmente, Hufflepuff buscaba alumnos que simplemente quisieran pertenecer a esa casa, aunque actualmente busca alumnos leales, honestos y que no teman el trabajo pesado. La fundadora es nada menos que la bruja Helga Hufflepuff, amiga desde la infancia de Rowena Ravenclaw. Helga fue una bruja muy noble, amigable y la principal impulsora de que Hogwarts aceptase a alumnos nacidos de muggles. La principal reliquia de la casa es la copa de Helga Hufflepuff. El símbolo de la casa es un tejón negro y sus colores representativos son el amarillo y el negro carbón.</p>
+            </div>
+            : biggerNumber === muggle ? 
+            ""
+            : ""}
+          </div>
+      </Collapse>
+      <div style={{ display: "flex", alignItems: "center", borderTop: "0.5px solid #cfcfcf", marginTop: "10px"}}>
+     <Typography variant="body2" color="textSecondary" component="p"> Afinidad con el resto de casas:
+     </Typography>
+      <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expandedSecond
+          })}
+          onClick={handleExpandClickSecond}
+          aria-expanded={expandedSecond}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </div>
+      <Collapse in={expandedSecond} timeout="auto" unmountOnExit>
+          <div className={classes.wrap}>
+            <p>Gryffindor: {gryffindor}%</p>
+            <p>Slytherin: {slytherin}%</p>
+            <p>Ravenclaw: {ravenclaw}%</p>
+            <p>Hufflepuff: {hufflepuff}%</p>
+            <p>Muggle: {muggle}%</p>
+          </div>
+      </Collapse>
+      </> : ""}
+      </div>
+    </Card>
+
+          <Button  variant="contained" color="primary" style={{  marginTop: "20px" }} onClick={()=> {
             setHidden(true);
             setCurrentPage(0);
             setGryffindor(0);
@@ -204,13 +367,15 @@ return (
             setMuggle(0);
             setLoading(false);
             setGoQuiz(false)
-          }}>Repetir Test</Button>
+          }}>
+            Repetir Test
+          </Button>
     </>
 )
- : ""}
+ : "" }
 
-    </div>
-</>
+    </Container>
+    </Box>
 )
 }
 
