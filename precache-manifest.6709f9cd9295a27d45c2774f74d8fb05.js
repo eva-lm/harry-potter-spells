85aef0fc0dafeb99ffb147e73394997d35a21c08
John Doe <eva.lm1810@gmail.com>
2021-08-05 22:07:09 +0200

    Updates

---
+++ precache-manifest.6709f9cd9295a27d45c2774f74d8fb05.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd56e0e88447a6ca600c67b96773219c",
+    "url": "/harry-potter-spells/index.html"
+  },
+  {
+    "revision": "51145746032a335717e8",
+    "url": "/harry-potter-spells/static/css/main.e0094519.chunk.css"
+  },
+  {
+    "revision": "879d16f4f957a02fb912",
+    "url": "/harry-potter-spells/static/js/2.94d504ca.chunk.js"
+  },
+  {
+    "revision": "34e08232f7e793d10d146b598e6a9958",
+    "url": "/harry-potter-spells/static/js/2.94d504ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51145746032a335717e8",
+    "url": "/harry-potter-spells/static/js/main.7e5ea89f.chunk.js"
+  },
+  {
+    "revision": "031a742be4bb5e6e4803",
+    "url": "/harry-potter-spells/static/js/runtime-main.316dca62.js"
+  },
+  {
+    "revision": "f8c84d5dba0bd6430e123b657ff39091",
+    "url": "/harry-potter-spells/static/media/desaprobacion.f8c84d5d.png"
+  },
+  {
+    "revision": "34a2872be5229d6271da634bd04aa926",
+    "url": "/harry-potter-spells/static/media/glasses-harry.34a2872b.png"
+  },
+  {
+    "revision": "b44a2e79a3d57498e88ec1a6e54543be",
+    "url": "/harry-potter-spells/static/media/gryffindor.b44a2e79.jpg"
+  },
+  {
+    "revision": "13b4c5bf8fb0dea9cef1118afa632c3c",
+    "url": "/harry-potter-spells/static/media/harrypotter.13b4c5bf.gif"
+  },
+  {
+    "revision": "01ed5de92526edafdeaba7afe983d1a8",
+    "url": "/harry-potter-spells/static/media/hufflepuff.01ed5de9.jpg"
+  },
+  {
+    "revision": "01aa512655fcedf39d04cc3e26ee8bab",
+    "url": "/harry-potter-spells/static/media/muggle.01aa5126.jpg"
+  },
+  {
+    "revision": "b3c72e98cd8e22e892b1d2c7efbf4179",
+    "url": "/harry-potter-spells/static/media/ravenclaw.b3c72e98.jpg"
+  },
+  {
+    "revision": "8231d486856c38ba51e8298db5c81789",
+    "url": "/harry-potter-spells/static/media/slytherin.8231d486.jpg"
+  },
+  {
+    "revision": "10f6bdef1553bf2dfdaee59d23cc17a3",
+    "url": "/harry-potter-spells/static/media/sombrero.10f6bdef.png"
+  },
+  {
+    "revision": "df8a084e9c58bff957768458844c0861",
+    "url": "/harry-potter-spells/static/media/song.df8a084e.mp3"
+  }
+]);
